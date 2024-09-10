73d197987138783a5b9fcc3194a74b7561003553
caiocroccia22 <caiocroccia22@gmail.com>
2024-09-10 20:38:28 -0300

    feat: Arising animation imagem

--- main.js
+++ main.js
@@ -10,17 +10,23 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li></li>');
+        const novoItem = $('<li style="display: none"></li>');
         
         //Adicionando a imagem ao novo item
         $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
         
         //Adicionando o overlay ao novo item
         $(`<div class="overlay-imagem-link">
-                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real" </a>
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a>
             </div>`).appendTo(novoItem);
 
         //Adicionando o novo item a lista       
         $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn();
+
+        //Limpando o campo
+        $('#endereco-imagem-nova').val(' ');
+
+        
         })
 });     
